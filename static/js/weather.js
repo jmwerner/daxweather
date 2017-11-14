6f341bdf34b9ecd12a9327cacde4da94ecba1822
Jeremy Werner <jmwerner1123@me.com>
2017-11-13 21:11:27 -0800

    Update js to reference images correctly

--- static/js/weather.js
+++ static/js/weather.js
@@ -9,20 +9,15 @@ $(function ()
 
 		$.get("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
 		{
+			console.log(data)
 			var description = data.weather[0].description;
 			var icon = "static/img/" + data.weather[0].icon + ".jpg";
-			var weathertext = "static/words/" + data.weather[0].icon + ".png";
 			var celcius = Math.round(data.main.temp - 273.15);
 			var fahrenheit = Math.round(celcius * 9/5 + 32);
 
 			$("h2").text(description);
 			$("body").css( "backgroundImage", "url(" + icon + ")");
-			$("body").css( ".weathertext", "url(" + weathertext + ")");
 			$("#f").text(fahrenheit);
-			$("#c").text(celcius);
-
-			var doge_img = "url(./img/words/" + data.weather[0].icon + ".png)";
-
 
 		});
 
