07d16b6912aca00fd9b02db2017421021fd38f7f
Jeremy Werner <jmwerner1123@me.com>
2017-12-03 22:24:04 -0800

    Add dax pictures and have picture change based on weather closes #4

--- static/js/weather.js
+++ static/js/weather.js
@@ -9,20 +9,20 @@ $(function ()
 
 		$.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
 		{
-			console.log(data)
 			var description = data.weather[0].description;
 			var icon = "static/img/" + data.weather[0].icon + ".jpg";
+            var face_picture = "static/img/dax/" + data.weather[0].icon + ".png";
 			var celcius = Math.round(data.main.temp - 273.15);
 			var fahrenheit = Math.round(celcius * 9/5 + 32);
 			var city_name = data.name;
 			var wind_speed = data.wind.speed;
 
 			$("#current_conditions").text(description);
-			$("body").css( "backgroundImage", "url(" + icon + ")");
+			$("body").css("backgroundImage", "url(" + icon + ")");
 			$("#f").text(fahrenheit);
 			$("#speed").text(wind_speed);
-			$("#city").text(city_name)
-
+			$("#city").text(city_name);
+            $("#face_picture").attr("src", face_picture);
             $('.wind_vector').each(function() {
                 var deg = data.wind.deg;
                 var rotate = 'rotate(' + deg + 'deg)';
