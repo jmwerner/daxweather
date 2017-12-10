43736ecd300c52d527f51ed7cfc226a643681e74
Jeremy Werner <jmwerner1123@me.com>
2017-12-10 10:30:47 -0800

    New layout

--- README.md
+++
@@ -1,8 +0,0 @@
-DAXWEATHER
-==========
-
-Sources of inspiration and/or code robbing
-* [drakeweather](http://drakeweather.com)
-* [dogeweather](http://dogeweather.com)
-* [varunweather](https://github.com/michaeltli/varunweather)
-
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -4,20 +4,19 @@ $(function ()
 	$.get("https://ipinfo.io", function(response)
 	{
 		var location = response.city + "," + response.country;
-        console.log(location)
 		$.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
 		{
 			var description = data.weather[0].description;
-			var icon = "static/img/" + data.weather[0].icon + ".jpg";
-            var face_picture = "static/img/dax/" + data.weather[0].icon + ".png";
+			var icon = "assets/img/" + data.weather[0].icon + ".jpg";
+            var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
 			var celcius = Math.round(data.main.temp - 273.15);
 			var fahrenheit = Math.round(celcius * 9/5 + 32);
 			var city_name = data.name;
 			var wind_speed = data.wind.speed;
 
 			$("#current_conditions").text(description);
-			$("body").css("backgroundImage", "url(" + icon + ")");
-			$("#f").text(fahrenheit);
+            $("#header").css("background-image", "url(" + icon + ")");            
+			$("#temperature").text(fahrenheit);
 			$("#speed").text(wind_speed);
 			$("#city").text(city_name);
             $("#face_picture").attr("src", face_picture);
@@ -46,16 +45,16 @@ $(document).ready(function(){
         $.get("https://api.openweathermap.org/data/2.5/weather?id=" + location_id + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
         {
             var description = data.weather[0].description;
-            var icon = "static/img/" + data.weather[0].icon + ".jpg";
-            var face_picture = "static/img/dax/" + data.weather[0].icon + ".png";
+            var icon = "assets/img/" + data.weather[0].icon + ".jpg";
+            var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
             var celcius = Math.round(data.main.temp - 273.15);
             var fahrenheit = Math.round(celcius * 9/5 + 32);
             var city_name = data.name;
             var wind_speed = data.wind.speed;
 
             $("#current_conditions").text(description);
-            $("body").css("backgroundImage", "url(" + icon + ")");
-            $("#f").text(fahrenheit);
+            $("#header").css("background-image", "url(" + icon + ")");
+            $("#temperature").text(fahrenheit);
             $("#speed").text(wind_speed);
             $("#city").text(city_name);
             $("#face_picture").attr("src", face_picture);
