9af2756fa54c7775a80dfa64595446d9ccd807ed
Jeremy Werner <jmwerner1123@me.com>
2017-12-10 22:17:15 -0800

    Add tighter vertical alignment between sections

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -36,7 +36,7 @@ $(function ()
 	}, "jsonp");
 });
 
-
+// Non-mobile dropdown menu
 $(document).ready(function(){
     $('.city_selection').on('click', function(){
 
@@ -72,3 +72,51 @@ $(document).ready(function(){
         });
     });
 });
+
+// Mobile dropdown menu
+$(document).ready(function(){
+    $('.link.depth-1').on('click', function(){
+        var cityMap = {};
+        cityMap['<span class="indent-1"></span>Sioux Falls'] = '5231851';
+        cityMap['<span class="indent-1"></span>New London'] = '4400423';
+        cityMap['<span class="indent-1"></span>Menlo Park'] = '5372223';
+        cityMap['<span class="indent-1"></span>Papillion'] = '5074792';
+        cityMap['<span class="indent-1"></span>Houston'] = '2646507';
+        cityMap['<span class="indent-1"></span>Aberdeen'] = '4346913';
+
+        var location_id = cityMap[this.innerHTML];
+
+        $.get("https://api.openweathermap.org/data/2.5/weather?id=" + location_id + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
+        {
+            var description = data.weather[0].description;
+            var icon = "assets/img/" + data.weather[0].icon + ".jpg";
+            var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
+            var celcius = Math.round(data.main.temp - 273.15);
+            var fahrenheit = Math.round(celcius * 9/5 + 32);
+            var city_name = data.name;
+            var wind_speed = data.wind.speed;
+
+            $("#current_conditions").text(description);
+            $("#header").css("background-image", "url(" + icon + ")");
+            $("#temperature").text(fahrenheit);
+            $("#speed").text(wind_speed);
+            $("#city").text(city_name);
+            $("#face_picture").attr("src", face_picture);
+            $('.wind_vector').each(function() {
+                var deg = data.wind.deg;
+                var rotate = 'rotate(' + deg + 'deg)';
+                $(this).css({ 
+                    '-webkit-transform': rotate,
+                    '-moz-transform': rotate,
+                    '-o-transform': rotate,
+                    '-ms-transform': rotate,
+                    'transform': rotate 
+                });
+            });
+        });
+    });
+});
+
+
+
+
