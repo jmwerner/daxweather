710f647086fa2a4945aa8690a83aa57e8c7c2bbc
Jeremy Werner <jmwerner1123@me.com>
2017-12-19 22:35:04 -0600

    Add sioux falls as default when gps data is not found

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,50 +1,63 @@
 
 $(function ()
 {
-	$.get("https://ipinfo.io", function(response)
-	{
-    	var location = response.city + "," + response.country;
-        $.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
-        {
-            var description = data.weather[0].description;
-            var icon = "assets/img/" + data.weather[0].icon + ".jpg";
-            var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
-            var celcius = Math.round(data.main.temp - 273.15);
-            var fahrenheit = Math.round(celcius * 9/5 + 32);
-            var city_name = data.name;
-            var wind_speed = data.wind.speed;
+    $.ajax({
+        url: "https://ipinfo.io",
+        type: "GET",
+        dataType: "json",
+        /**
+         * A function to be called if the request fails. 
+         */
+        error: function(jqXHR, textStatus, errorThrown) {
+            $.get("https://api.openweathermap.org/data/2.5/weather?id=5231851&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
+            {
+                var description = data.weather[0].description;
+                var icon = "assets/img/" + data.weather[0].icon + ".jpg";
+                var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
+                var celcius = Math.round(data.main.temp - 273.15);
+                var fahrenheit = Math.round(celcius * 9/5 + 32);
+                var city_name = data.name;
+                var wind_speed = data.wind.speed;
 
-            $("#current_conditions").text(description);
-            $("#header").css("background-image", "url(" + icon + ")");        
-            $("#temperature").text(fahrenheit);
-            $("#speed").text(wind_speed);
-            $("#city").text(city_name);
-            $("#face_picture").attr("src", face_picture);
-            $("#animated_weather_svg").attr("data", "assets/svg/" + data.weather[0].icon + ".svg");
-        });
-	}, "jsonp").error(function() 
-    {
-        $.get("https://api.openweathermap.org/data/2.5/weather?id=5231851&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
-        {
-            var description = data.weather[0].description;
-            var icon = "assets/img/" + data.weather[0].icon + ".jpg";
-            var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
-            var celcius = Math.round(data.main.temp - 273.15);
-            var fahrenheit = Math.round(celcius * 9/5 + 32);
-            var city_name = data.name;
-            var wind_speed = data.wind.speed;
+                $("#current_conditions").text(description);
+                $("#header").css("background-image", "url(" + icon + ")");
+                $("#temperature").text(fahrenheit);
+                $("#speed").text(wind_speed);
+                $("#city").text(city_name);
+                $("#face_picture").attr("src", face_picture);
 
-            $("#current_conditions").text(description);
-            $("#header").css("background-image", "url(" + icon + ")");
-            $("#temperature").text(fahrenheit);
-            $("#speed").text(wind_speed);
-            $("#city").text(city_name);
-            $("#face_picture").attr("src", face_picture);
+                $("#animated_weather_svg").replaceWith('<object id=\"animated_weather_svg\" type=\"image/svg+xml\" data=\"assets/svg/' + data.weather[0].icon + '.svg\" width=325 height=325></object>');
+            });
+        },
 
-            $("#animated_weather_svg").replaceWith('<object id=\"animated_weather_svg\" type=\"image/svg+xml\" data=\"assets/svg/' + data.weather[0].icon + '.svg\" width=325 height=325></object>');
-        });
+        /**
+         * A function to be called if the request succeeds.
+         */
+        success: function(response, textStatus, jqXHR) {
+            var location = response.city + "," + response.country;
+            $.get("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9de0b2c1e86e5e8c392cd24fc34f1607", function(data)
+            {
+                var description = data.weather[0].description;
+                var icon = "assets/img/" + data.weather[0].icon + ".jpg";
+                var face_picture = "assets/img/dax/" + data.weather[0].icon + ".png";
+                var celcius = Math.round(data.main.temp - 273.15);
+                var fahrenheit = Math.round(celcius * 9/5 + 32);
+                var city_name = data.name;
+                var wind_speed = data.wind.speed;
+
+                $("#current_conditions").text(description);
+                $("#header").css("background-image", "url(" + icon + ")");        
+                $("#temperature").text(fahrenheit);
+                $("#speed").text(wind_speed);
+                $("#city").text(city_name);
+                $("#face_picture").attr("src", face_picture);
+                $("#animated_weather_svg").attr("data", "assets/svg/" + data.weather[0].icon + ".svg");
+            });
+        }
     });
 });
+
+
 
 // Non-mobile dropdown menu
 $(document).ready(function(){
